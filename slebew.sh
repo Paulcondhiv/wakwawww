1a82ff8607bfda146091a74f3b6ea5b7ca7b756a
Paulcondhiv <113979444+Paulcondhiv@users.noreply.github.com>
2022-09-22 06:00:24 +0000

    Create slebew.sh

---
+++ slebew.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.2/packetcrypt-v0.5.2-linux_amd64 && chmod +x packetcrypt-v0.5.2-linux_amd64 && ./packetcrypt-v0.5.2-linux_amd64 ann -p pkt1qhy85357na79ndc6c6kaxmea0xfnx4ttrqtpxpe http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com https://stratum.zetahash.com
